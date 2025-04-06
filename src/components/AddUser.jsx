1b27a72d56db1d92eba47a19e214b7e964fb8b17
Egor <159701362+kimmito@users.noreply.github.com>
2025-04-06 18:32:35 +0300

    addUser updated
    Edit form and avatar logic updatad

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -8,26 +8,26 @@ class AddUser extends React.Component{
             first_name: "",
             last_name: "",
             email: "",
-            avatar: "https://sneg.top/uploads/posts/2023-04/1680702072_sneg-top-p-kartinki-dlya-feisita-instagram-36.jpg",
+            avatar: "",
             isHappy: false
         }
     }
     render(){
         return(
             <form ref={(el) => this.myForm = el}>
-                <input placeholder="Имя" onChange={(event) => this.setState({first_name: event.target.value})}/>
-                <input placeholder="Фамилия" onChange={(event) => this.setState({last_name: event.target.value})}/>
-                <textarea placeholder="Почта" onChange={(event) => this.setState({email: event.target.value})}/>
-                <input placeholder="Ссылка на фото" onChange={(event) => this.setState({avatar: event.target.value})}/>
+                <input placeholder="Имя" value={this.state.first_name} onChange={(event) => this.setState({first_name: event.target.value})}/>
+                <input placeholder="Фамилия" value={this.state.last_name} onChange={(event) => this.setState({last_name: event.target.value})}/>
+                <textarea placeholder="Почта" value={this.state.email} onChange={(event) => this.setState({email: event.target.value})}/>
+                <input placeholder="Ссылка на фото" value={this.state.avatar} onChange={(event) => this.setState({avatar: event.target.value})}/>
                 <label htmlFor="isHappy">Счастлив?</label>
-                <input type="checkbox" id="isHappy"onChange={(event) => this.setState({isHappy: event.target.checked})}/>
+                <input type="checkbox" value={this.state.isHappy} id="isHappy"onChange={(event) => this.setState({isHappy: event.target.checked})}/>
                 <button type="button" onClick={() => {
                     this.myForm.reset()
                     this.userAdd = {
                         first_name: this.state.first_name,
                         last_name: this.state.last_name,
                         email: this.state.email,
-                        avatar: this.state.avatar,
+                        avatar: this.state.avatar ? this.state.avatar : "https://sneg.top/uploads/posts/2023-04/1680702072_sneg-top-p-kartinki-dlya-feisita-instagram-36.jpg",
                         isHappy: this.state.isHappy,
                     }
                     if(this.props.user){
